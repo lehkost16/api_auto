95a3e1eb2f742d88545b0a3a5930836c2cabb7b0
Grecale <lehkost16@outlook.com>
2023-04-09 20:23:22 +0800

    help

--- api/user_api.py
+++ api/user_api.py
@@ -1,2 +1,10 @@
-def add():
-    return
+import pytest
+
+@pytest.mark.usefixtures("get_token")
+class Test_login():
+    def test_fix(self):
+        print('测试登陆夹具')
+
+
+if __name__ == "__main__":
+    pytest.main()
--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 from common import base_data
@@ -8,28 +9,39 @@ from common import base_data
 # pur_passwd_bytes = base_data.pur_passwd.encode('utf-8')
 #
 # pur_passwd = base_data.pur_passwd
-
+@pytest.fixture(scope='class')
 def get_token():
     user_data = {
         "username": base_data.pur_account,
         "password": base_data.pur_passwd,
         "imageCode": "0000"
     }
+    # session = requests.session()
+    resp = requests.post(base_data.base_url + "/userauth/cloLogin/purchaser_login", json=user_data)  ##获取登陆接口
+    # session.post(base_data.base_url + "/userauth/cloLogin/purchaser_login", json=user_data)
+    cookies = resp.cookies
+    print('cookie', cookies)
 
-    resp = requests.post(base_data.base_url + "/userauth/cloLogin/purchaser_login", json=user_data)
+    response_index = requests.get(base_data.base_url, cookies=cookies)
+    print('Response Status', response_index.status_code)
+    print('Response URL', response_index.url)
+
     # print(resp.cookies)
-    cookiejar = resp.cookies.get_dict()
-    print('first cookie', cookiejar)
-    x = resp.json()
-    print('there is a token in resp', resp.json())
-    print('the data of token', x['user_data']['token'])
-    cookiejar['Authorization'] = x['user_data']['token']
-    print('the cookie has add a token', cookiejar)
-    cookie_string = ';'.join(str(x) + '=' + str(y) for x, y in cookiejar.items())
-    headers = {
-        'Cookie': cookie_string
-    }
-    print(cookie_string)
-    print(headers)
+    # cookiejar = resp.cookies.get_dict()
+    # print('first cookie', cookiejar)
+    # x = resp.json()
+    # print('there is a token in resp', resp.json())
+    # print('the data of token', x['user_data']['token'])
+    # cookiejar['Authorization'] = x['user_data']['token']
+    # print('the cookie has add a token', cookiejar)
+    # cookie_string = ';'.join(str(x) + '=' + str(y) for x, y in cookiejar.items())
+    # headers = {
+    #     'Cookie': cookie_string
+    # }
+    # print(cookie_string)
+    # print(headers)
 
-get_token()
+
+# get_token()
+# @pytest.mark.usefixtures('get_token')
+
