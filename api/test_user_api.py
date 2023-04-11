e2e99e91521cbda508afe62755204df66457b5a7
lehkost16 <lehkost16@outlook.com>
2023-04-11 16:28:36 +0800

    git

--- api/test_user_api.py
+++ api/test_user_api.py
@@ -1,14 +1,12 @@
 import pytest
 import requests
-
-# from ..common import base_data
 from api_auto.common import base_data
 
-    # def test_fix(self):
-    #     print('测试登陆夹具')
-def test_creat(get_cookies,get_headers):
+# def test_fix(self):
+#     print('测试登陆夹具')
+def test_create(get_cookies,get_headers):
     print(get_cookies)
-    resp = requests.post(base_data.base_url +'/purchaser/trade/order/createSubmit', data=base_data.creatOrder,headers=get_headers,cookies=get_cookies)
+    resp = requests.post(base_data.base_url +'/purchaser/trade/order/createSubmit', data=base_data.creatOrder,cookies=get_cookies)
     # print(resp.cookies)
     print(resp.url)
     print(resp.status_code)
