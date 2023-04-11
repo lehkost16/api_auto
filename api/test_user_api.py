66f915978b3b1f82b099a72619982cdcc0260516
lehkost16 <lehkost16@outlook.com>
2023-04-11 16:29:22 +0800

    123

--- api/test_user_api.py
+++ api/test_user_api.py
@@ -4,7 +4,7 @@ from api_auto.common import base_data
 
 # def test_fix(self):
 #     print('测试登陆夹具')
-def test_create(get_cookies,get_headers):
+def test_create(get_cookies):
     print(get_cookies)
     resp = requests.post(base_data.base_url +'/purchaser/trade/order/createSubmit', data=base_data.creatOrder,cookies=get_cookies)
     # print(resp.cookies)
