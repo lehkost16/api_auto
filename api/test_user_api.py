2c17d03f7e7cf79af439a1ea0f24d47b4c409dfe
Grecale <lehkost16@outlook.com>
2023-04-12 21:40:07 +0800

    123

--- api/test_user_api.py
+++ api/test_user_api.py
@@ -3,25 +3,30 @@ import pytest
 import requests
 from api_auto.common import base_data
 from api_auto.common import MyEncoder
+
+
 # def test_fix(self):
 #     print('测试登陆夹具')
-def test_createSettlementorder(get_cookies,get_headers):
+def test_createSettlementorder(get_cookies, get_headers):
     # print(get_cookies)
     print(get_headers)
-    resp = requests.post(base_data.base_url +'/purchaser/trade/invoiceOrder/asyncStandardCreate', json=base_data.createSettlementorder,cookies=get_cookies)
+    resp = requests.post(base_data.base_url + '/purchaser/trade/invoiceOrder/asyncStandardCreate',
+                         json=base_data.createSettlementorder, cookies=get_cookies)
     # print(resp.cookies)
     print(resp.text)
     print(resp.status_code)
-def get_data():
-    return base_data.createOrder
-def test_createOrder(get_cookies,get_headers):
+
+
+def test_createOrder(get_cookies, get_headers):
     # print(get_cookies)
-    # headers = {'Content-Type': 'application/json'}
-    data1=json.dumps(get_data(),ensure_ascii=False, sort_keys=True, indent=4)
+    headers = {'Content-Type': 'application/json'}
+    data = json.dumps(base_data.createOrder, ensure_ascii=False, sort_keys=True, indent=4)
     # print(get_headers)
-    resp = requests.post(base_data.base_url +'/purchaser/trade/order/createSubmit', json=data1,cookies=get_cookies)
+    resp = requests.post(base_data.base_url + '/purchaser/trade/order/createSubmit', json=data, cookies=get_cookies)
     # print(resp.cookies)
     print(resp.text)
     print(resp.status_code)
+
+
 if __name__ == "__main__":
     pytest.main()
